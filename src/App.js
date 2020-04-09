55660fedc92bb49cb37c7caaaa5295ffc0801108
Alvaro Israel Nunes Leite <israel.nunes.leite@gmail.com>
2020-04-09 13:56:26 -0300

    chore: Created the display of the calculator.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
-import React from 'react';
-
-import {
-  SafeAreaView,
-  View,
-  StatusBar,
-  StyleSheet,
-} from 'react-native';
-
+import React, {useState} from 'react';
+import {SafeAreaView, View, StatusBar, StyleSheet} from 'react-native';
 import Button from './pages/button/button';
+import Display from './pages/display/display';
 
-export default function App() {
-
-  return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#525152"/>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.buttons}>
-          <Button label='C' operationTop onClick={()=>{}} />
-          <Button label='+/-' operationTop onClick={()=>{}} />
-          <Button label='%' operationTop onClick={()=>{}} />
-          <Button label='÷' operation onClick={()=>{}} />
-          <Button label='7' onClick={()=>{}} />
-          <Button label='8' onClick={()=>{}} />
-          <Button label='9' onClick={()=>{}} />
-          <Button label='*' operation  onClick={()=>{}} />
-          <Button label='4' onClick={()=>{}} />
-          <Button label='5' onClick={()=>{}} />
-          <Button label='6' onClick={()=>{}} />
-          <Button label='-' operation onClick={()=>{}} />
-          <Button label='1' onClick={()=>{}} />
-          <Button label='2' onClick={()=>{}} />
-          <Button label='3' onClick={()=>{}} />
-          <Button label='+' operation onClick={()=>{}} />
-          <Button label='0' double  onClick={()=>{}} />
-          <Button label=',' onClick={()=>{}} />
-          <Button label='=' operation onClick={()=>{}} />
-        </View>
-      </SafeAreaView>
-    </>
-  );
-}
-
-const styles = StyleSheet.create({
+const css = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#525152',
@@ -51,3 +13,57 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
 });
+
+export default function App() {
+
+  const [displayValue, setDisplayValue] = useState('0');
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#525152"/>
+      <SafeAreaView style={css.container}>
+        <Display value={displayValue}/>
+        <View style={css.buttons}>
+          <Button label='C' operationTop onClick={() => {
+          }}/>
+          <Button label='+/-' operationTop onClick={() => {
+          }}/>
+          <Button label='%' operationTop onClick={() => {
+          }}/>
+          <Button label='÷' operation onClick={() => {
+          }}/>
+          <Button label='7' onClick={() => {
+          }}/>
+          <Button label='8' onClick={() => {
+          }}/>
+          <Button label='9' onClick={() => {
+          }}/>
+          <Button label='x' operation onClick={() => {
+          }}/>
+          <Button label='4' onClick={() => {
+          }}/>
+          <Button label='5' onClick={() => {
+          }}/>
+          <Button label='6' onClick={() => {
+          }}/>
+          <Button label='-' operation onClick={() => {
+          }}/>
+          <Button label='1' onClick={() => {
+          }}/>
+          <Button label='2' onClick={() => {
+          }}/>
+          <Button label='3' onClick={() => {
+          }}/>
+          <Button label='+' operation onClick={() => {
+          }}/>
+          <Button label='0' double onClick={() => {
+          }}/>
+          <Button label=',' onClick={() => {
+          }}/>
+          <Button label='=' operation onClick={() => {
+          }}/>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
---
+++ src/pages/display/index.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  display: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    backgroundColor: '#525152',
+    alignItems: 'flex-end',
+  },
+  displayValue: {
+    fontSize: 60,
+    color: '#fff',
+  },
+});
